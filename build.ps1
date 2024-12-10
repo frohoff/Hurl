5d13fadc88fa9bf74b6c359cd4c68d5586fbf330
Chris Frohoff <frohoff@yahoo.com>
2024-12-10 15:11:06 -0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,12 +3,12 @@ $INNO_SETUP_COMPILER = "C:/Program Files (x86)/Inno Setup 6/ISCC.exe"
 
 Write-Output "Building Launcher...."
 cd ./Source/Launcher
-cargo build --release
+cargo build --release --target aarch64-pc-windows-msvc
 cd ../..
 
 Write-Output "Building Hurl...."
 dotnet restore
-dotnet publish -c Release -r win-x64 --no-self-contained --output $OUTPUT_PATH ./Hurl.sln
+dotnet publish -c Release -r win-arm64 --no-self-contained --output $OUTPUT_PATH ./Hurl.sln
 
 Write-Output "Building Installer...."
 & $INNO_SETUP_COMPILER ./Utils/installer.iss
